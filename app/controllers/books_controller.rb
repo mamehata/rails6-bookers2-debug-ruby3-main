cee06f17f555e5f9eec5a3ccc2ffe3ad97b7dac1
EC2 Default User <ec2-user@ip-172-31-32-132.ap-northeast-1.compute.internal>
2022-12-02 06:01:47 +0000

    [Add]いいね、コメント、部分テンプレート追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :correct_user, only: [:edit]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def show
     @comment = BookComment.new
