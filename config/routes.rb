41617f46d3c03c9f05e47dd2c224cc223efd178a
EC2 Default User <ec2-user@ip-172-31-32-132.ap-northeast-1.compute.internal>
2022-12-03 17:23:41 +0000

    [Add]フォロー/フォロワー機能実装途中

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about"=>"homes#about"
 
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    member do
+      get :followings, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
+  end
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
