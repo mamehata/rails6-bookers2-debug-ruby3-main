456bc3eb60bd167f0b5b024aaa48c61058754cbf
EC2 Default User <ec2-user@ip-172-31-32-132.ap-northeast-1.compute.internal>
2022-12-04 03:27:09 +0000

    [Finish]フォロー/フォロワー機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,12 +25,12 @@ class UsersController < ApplicationController
   end
 
   def followings
-    user = User.find(params[:user_id])
+    user = User.find(params[:id])
     @users = user.followings
   end
 
   def followers
-    user = User.find(params[:user_id])
+    user = User.find(params[:id])
     @users = user.followers
   end
 
