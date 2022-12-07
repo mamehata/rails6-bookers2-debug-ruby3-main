95075f935cab972342d4f319e16246dc88110fe9
EC2 Default User <ec2-user@ip-172-31-32-132.ap-northeast-1.compute.internal>
2022-12-07 10:02:42 +0000

    [Add]groups/new追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,7 @@ class Group < ApplicationRecord
   has_many :groups, dependent: :destroy
   has_one_attached :group_image
 
-
+  def get_group_image
+    (group_image.attached?) ? group_image : 'no_image.jpg'
+  end
 end
