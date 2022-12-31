6cfc1e0cf31f07f8e383ffc666012a19f234cf94
EC2 Default User <ec2-user@ip-172-31-46-47.ap-northeast-1.compute.internal>
2022-12-31 18:02:42 +0000

    [finish]ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,13 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
+  def self.guest
+    find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'guestuser'
+    end
+  end
+
   def self.looks(search,word)
     if search == "perfect_match"
       @user = User.where("name LIKE?","#{word}")
